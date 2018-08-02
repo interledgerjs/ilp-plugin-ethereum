fc3f5070169e4c8b2bccda2177cbd8f34cc2c224
Austin King <austinking@college.harvard.edu>
2018-08-02 14:16:52 -0700

    Default framework and packages installed

--- test/pluginSpec.js
+++ test/pluginSpec.js
@@ -7,13 +7,21 @@ const HDWalletProvider = require('truffle-hdwallet-provider')
 const SECRET = "lazy glass net matter square melt fun diary network bean play deer"
 const PROVIDER_URL = "https://ropsten.infura.io/T1S8a0bkyrGD7jxJBgeH"
 
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const assert = chai.assert
+const sinon = require('sinon')
+const debug = require('debug')('ilp-plugin-eth-asym-server:test')
+const EventEmitter = require('events')
+
 function createPlugin(opts = {}) {
   return new PluginEthAsymServer(Object.assign({
     prefix: 'test.example',
     provider: () => new HDWalletProvider(SECRET, PROVIDER_URL, 0),
     account: '0xad217fb704030c8465682c3b1a6ac3ed56bff8ab',
     minimumChannelAmount: 1000000,
-    maxPacketAmount: 1000000, 
+    maxPacketAmount: 1000000,
     _store: new Store(null, 'test.example'),
     debugHostIldcpInfo: {
       clientAddress: 'test.example',
@@ -25,21 +33,20 @@ function createPlugin(opts = {}) {
 
 describe('pluginSpec', () => {
 
-  describe('constructor', function () {
-    it('should throw if currencyScale is neither undefined nor a number', function () {
-      assert.throws(() => createPlugin({ currencyScale: 'oaimwdaiowdoamwdaoiw' }),
-        /currency scale must be a number if specified/)
-    })
-  })
-
   beforeEach(async function () {
+      this.timeout(10000)
+      this.sinon = sinon.sandbox.create()
+      this.plugin = createPlugin()
   })
 
   afterEach(async function () {
+      this.sinon.restore()
+      await this.plugin.disconnect()
   })
 
   describe('validate channel details', () => {
     beforeEach(function () {
+
     })
 
     it('should not accept a channel with a short settle delay', function () {
