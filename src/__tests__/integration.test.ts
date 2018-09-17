dd52235fcfefffa505e25ddb6a6194de75656b04
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2018-09-17 17:50:41 -0400

    test: fix issues, use ava
    - due to https://github.com/facebook/jest/issues/3917, Jest assertions weren't working from within Stream event handlers
    - miscellaneous fixes

---
+++ src/__tests__/integration.test.ts
@@ -0,0 +1,147 @@
+import Web3 = require('web3')
+import * as IlpStream from 'ilp-protocol-stream'
+import * as getPort from 'get-port'
+import axios from 'axios'
+import EthereumPlugin = require('..')
+import { convert, Unit } from '../account'
+import BigNumber from 'bignumber.js'
+import test from 'ava'
+
+test('client streams data and money to server', async t => {
+  const web3 = new Web3('wss://ropsten.infura.io/ws')
+
+  const privateKeys = [
+    process.env.PRIVATE_KEY_A,
+    process.env.PRIVATE_KEY_B
+  ] as string[]
+
+  const addresses = privateKeys.map(key => web3.eth.accounts.wallet.add(key).address)
+
+  for (let address of addresses) {
+    // If the balance of the account goes below 5 ETH, top up from faucet
+    const balance = await web3.eth.getBalance(address)
+    if (convert(5, Unit.Eth, Unit.Wei).gt(balance)) {
+      const { data } = await axios.post(`https://faucet.metamask.io/`, address, {
+        headers: {
+          'Content-Type': 'application/rawdata'
+        }
+      })
+
+      // MetaMask faucet returns the tx hash
+      if (typeof data === 'string') {
+        await new Promise(resolve => {
+          // Check for the tx to be mined
+          const interval = setInterval(async () => {
+            const receipt = await web3.eth.getTransactionReceipt(data)
+
+            if (receipt && receipt.status) {
+              clearInterval(interval)
+              resolve()
+            }
+          }, 1000)
+        })
+      }
+    }
+  }
+
+  const AMOUNT_TO_SEND = convert('0.0023', Unit.Eth, Unit.Gwei)
+  const SENDER_SETTLE_TO = convert('0.002', Unit.Eth, Unit.Gwei)
+  const INCOMING_FEE = convert('0.00045', Unit.Eth, Unit.Gwei)
+  const RECEIVER_MAX_BALANCE = 0
+
+  let actualReceived = new BigNumber(0)
+
+  let serverConn: IlpStream.Connection
+  let serverStream: IlpStream.DataAndMoneyStream
+
+  let clientConn: IlpStream.Connection
+  let clientStream: IlpStream.DataAndMoneyStream
+
+  const port = await getPort()
+
+  const clientPlugin = new EthereumPlugin({
+    role: 'client',
+    ethereumPrivateKey: process.env.PRIVATE_KEY_A!,
+    ethereumProvider: 'wss://ropsten.infura.io/ws',
+    // @ts-ignore
+    server: `btp+ws://userA:secretA@localhost:${port}`,
+    outgoingChannelAmount: convert('0.01', Unit.Eth, Unit.Gwei),
+    balance: {
+      settleTo: SENDER_SETTLE_TO,
+      settleThreshold: convert('0.0019', Unit.Eth, Unit.Gwei)
+    }
+  })
+
+  const serverPlugin = new EthereumPlugin({
+    role: 'server',
+    ethereumPrivateKey: process.env.PRIVATE_KEY_B!,
+    ethereumProvider: 'wss://ropsten.infura.io/ws',
+    // @ts-ignore
+    debugHostIldcpInfo: {
+      assetCode: 'ETH',
+      assetScale: 9,
+      clientAddress: 'private.ethereum'
+    },
+    port,
+    maxPacketAmount: convert('0.0001', Unit.Eth, Unit.Gwei),
+    outgoingChannelAmount: convert('0.01', Unit.Eth, Unit.Gwei),
+    incomingChannelFee: INCOMING_FEE,
+    balance: {
+      maximum: RECEIVER_MAX_BALANCE
+    }
+  })
+
+  clientPlugin.registerMoneyHandler(Promise.resolve)
+  serverPlugin.registerMoneyHandler((amount: string) => {
+    actualReceived = actualReceived.plus(amount)
+  })
+
+  await serverPlugin.connect()
+  await clientPlugin.connect()
+
+  // Setup the receiver (Ethereum server, Stream server)
+  const streamServer = await IlpStream.createServer({
+    plugin: serverPlugin,
+    receiveOnly: true
+  })
+
+  await new Promise(async resolve => {
+    streamServer.once('connection', (conn: IlpStream.Connection) => {
+      serverConn = conn
+
+      serverConn.once('stream', (stream: IlpStream.DataAndMoneyStream) => {
+        stream.setReceiveMax(Infinity)
+        serverStream = stream
+
+        resolve()
+      })
+    })
+
+    // Setup the sender (Ethereum client, Stream client)
+    clientConn = await IlpStream.createConnection({
+      plugin: clientPlugin,
+      ...(streamServer.generateAddressAndSecret())
+    })
+
+    clientStream = clientConn.createStream()
+    clientStream.setSendMax(AMOUNT_TO_SEND)
+  })
+
+  serverStream!.on('money', () => {
+    const amountPrefunded = actualReceived.minus(serverConn.totalReceived)
+
+    t.true(amountPrefunded.gte(RECEIVER_MAX_BALANCE), 'amount prefunded to server is always at least the max balance')
+    t.true(amountPrefunded.lte(SENDER_SETTLE_TO), 'amount prefunded to server is never greater than settleTo amount')
+  })
+
+  await t.notThrowsAsync(serverStream!.receiveTotal(AMOUNT_TO_SEND), 'client streamed the total amount of packets to the server')
+
+  // Wait 10 seconds for sender to finish sending the remaining payment channel claims
+  await new Promise(resolve => {
+    setTimeout(resolve, 10000)
+  })
+
+  t.true(actualReceived.gte(AMOUNT_TO_SEND.minus(INCOMING_FEE)), 'server received at least as much money as the client sent')
+
+  await clientConn!.end()
+})
