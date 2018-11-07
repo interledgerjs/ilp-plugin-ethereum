71cb757a143dcb846906cfe3f8e346053d23d78b
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2018-11-07 14:16:18 -0500

    feat: simplify server and client wrapper
    - fetch/load account via functions passed to constructor
    - separation of concerns: accounts are no longer instantiated in plugin wrapper
    - add specific types for mini-accounts and btp options

---
+++ src/utils/client.ts
@@ -0,0 +1,81 @@
+import EthereumAccount, { requestId } from '../account'
+import BtpPlugin, {
+  BtpPacket,
+  BtpSubProtocol,
+  IlpPluginBtpConstructorOptions
+} from 'ilp-plugin-btp'
+import {
+  TYPE_MESSAGE,
+  MIME_APPLICATION_OCTET_STREAM
+} from 'btp-packet'
+import { PluginInstance, PluginServices } from './types'
+import { deserializeIlpPacket } from 'ilp-packet'
+
+export interface EthereumClientOpts extends IlpPluginBtpConstructorOptions {
+  getAccount: (accountName: string) => EthereumAccount
+  loadAccount: (accountName: string) => Promise<EthereumAccount>
+}
+
+export class EthereumClientPlugin extends BtpPlugin implements PluginInstance {
+  private getAccount: () => EthereumAccount
+  private loadAccount: () => Promise<EthereumAccount>
+
+  constructor ({
+    getAccount,
+    loadAccount,
+    ...opts
+  }: EthereumClientOpts, { log }: PluginServices) {
+    super(opts, { log })
+
+    this.getAccount = () => getAccount('peer')
+    this.loadAccount = () => loadAccount('peer')
+  }
+
+  _sendMessage (accountName: string, message: BtpPacket) {
+    return this._call('', message)
+  }
+
+  async _connect (): Promise<void> {
+    return (await this.loadAccount()).connect()
+  }
+
+  _handleData (from: string, message: BtpPacket): Promise<BtpSubProtocol[]> {
+    return this.getAccount().handleData(message)
+  }
+
+  _handleMoney (from: string, message: BtpPacket): Promise<BtpSubProtocol[]> {
+    return this.getAccount().handleMoney(message)
+  }
+
+  // Add hooks into sendData before and after sending a packet for
+  // balance updates and settlement, akin to mini-accounts
+  async sendData (buffer: Buffer): Promise<Buffer> {
+    const preparePacket = deserializeIlpPacket(buffer)
+
+    const response = await this._call('', {
+      type: TYPE_MESSAGE,
+      requestId: await requestId(),
+      data: {
+        protocolData: [{
+          protocolName: 'ilp',
+          contentType: MIME_APPLICATION_OCTET_STREAM,
+          data: buffer
+        }]
+      }
+    })
+
+    const ilpResponse = response.protocolData.find(p => p.protocolName === 'ilp')
+    if (ilpResponse) {
+      const responsePacket = deserializeIlpPacket(ilpResponse.data)
+      this.getAccount().handlePrepareResponse(preparePacket, responsePacket)
+      return ilpResponse.data
+    }
+
+    // TODO Should this return a REJECT packet?
+    return Buffer.alloc(0)
+  }
+
+  _disconnect (): Promise<void> {
+    return this.getAccount().disconnect()
+  }
+}
---
+++ src/utils/server.ts
@@ -0,0 +1,67 @@
+import EthereumAccount from '../account'
+import { PluginInstance, PluginServices } from './types'
+import MiniAccountsPlugin from 'ilp-plugin-mini-accounts'
+import { ServerOptions } from 'ws'
+import { IldcpResponse } from 'ilp-protocol-ildcp'
+import { BtpPacket, BtpSubProtocol } from 'ilp-plugin-btp'
+import { IlpPacket } from 'ilp-packet'
+
+export interface MiniAccountsOpts {
+  port?: number
+  wsOpts?: ServerOptions
+  debugHostIldcpInfo?: IldcpResponse
+  allowedOrigins?: string[]
+}
+
+export interface EthereumServerOpts extends MiniAccountsOpts {
+  getAccount: (accountName: string) => EthereumAccount
+  loadAccount: (accountName: string) => Promise<EthereumAccount>
+}
+
+export class EthereumServerPlugin extends MiniAccountsPlugin implements PluginInstance {
+  private getAccount: (address: string) => EthereumAccount
+  private loadAccount: (address: string) => Promise<EthereumAccount>
+
+  constructor ({
+    getAccount,
+    loadAccount,
+    ...opts
+  }: EthereumServerOpts, api: PluginServices) {
+    super(opts, api)
+
+    this.getAccount = (address: string) =>
+      getAccount(this.ilpAddressToAccount(address))
+    this.loadAccount = (address: string) =>
+      loadAccount(this.ilpAddressToAccount(address))
+  }
+
+  _sendMessage (accountName: string, message: BtpPacket) {
+    return this._call(this._prefix + accountName, message)
+  }
+
+  async _connect (address: string, message: BtpPacket): Promise<void> {
+    const account = await this.loadAccount(address)
+    return account.connect()
+  }
+
+  _handleCustomData = async (from: string, message: BtpPacket): Promise<BtpSubProtocol[]> => {
+    return this.getAccount(from).handleData(message)
+  }
+
+  async _handleMoney (from: string, message: BtpPacket): Promise<BtpSubProtocol[]> {
+    return this.getAccount(from).handleMoney(message)
+  }
+
+  // @ts-ignore TODO Different version of ilp-packet used in mini-accounts
+  _handlePrepareResponse = async (
+    destination: string,
+    responsePacket: IlpPacket,
+    preparePacket: IlpPacket
+  ) => {
+    return this.getAccount(destination).handlePrepareResponse(preparePacket, responsePacket)
+  }
+
+  async _close (from: string): Promise<void> {
+    return this.getAccount(from).disconnect()
+  }
+}
